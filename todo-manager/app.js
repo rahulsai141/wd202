a54ea675129106ce22dbb2d20e299faf795be5cd
Rahul Sai <rahulsai899@gmail.com>
2024-06-13 18:25:19 -0400

    Add a create Todo

--- todo-manager/app.js
+++ todo-manager/app.js
@@ -4,10 +4,11 @@ const app = express();
 const { Todo } = require('./models');
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.set('view engine', 'ejs');
 
-app.get('/index', async (request, response) => {
+app.get('/', async (request, response) => {
   const allTodos = await Todo.getTodos();
   const overDueTodos = await Todo.overdue();
   const dueDateTodos = await Todo.dueToday();
@@ -46,11 +47,12 @@ app.post('/todos', async (request, response) => {
   //response.send('Create a Todo', request.body);
 
   try {
-    const todo = await Todo.addTodo({
+    await Todo.addTodo({
       title: request.body.title,
       dueDate: request.body.dueDate,
     });
-    return response.json(todo);
+    //return response.json(todo);
+    return response.redirect('/');
   } catch (error) {
     console.log(error);
     return response.status(422).json(error);
